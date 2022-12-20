3c114aa49c6109e5456fa4572d71e36f3fe456fa
JatinRokde <109551379+JatinRokde@users.noreply.github.com>
2022-12-21 01:27:58 +0530

    Update Q5_CountSort.cpp

--- Assignment 1/Q5_CountSort.cpp
+++ Assignment 1/Q5_CountSort.cpp
@@ -1,8 +1,33 @@
-#include <iostream>
+#include <bits/stdc++.h>
+#include <string.h>
 using namespace std;
+#define RANGE 255
 
-int main(void)
+void countSort(char arr[])
 {
-    
+    char output[strlen(arr)];
+    int count[RANGE + 1], i;
+    memset(count, 0, sizeof(count));
+
+    for (i = 0; arr[i]; ++i)
+        ++count[arr[i]];
+ 
+    for (i = 1; i <= RANGE; ++i)
+        count[i] += count[i - 1];
+
+    for (i = 0; arr[i]; ++i) {
+        output[count[arr[i]] - 1] = arr[i];
+        --count[arr[i]];
+    }
+
+    for (i = 0; arr[i]; ++i)
+        arr[i] = output[i];
+}
+
+int main()
+{
+    char arr[] = "DataStructures";
+    countSort(arr);
+    cout << "Sorted character array is " << arr;
     return 0;
 }
